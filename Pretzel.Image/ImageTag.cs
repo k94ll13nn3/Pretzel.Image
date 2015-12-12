1991e965f834fcae52327163045c448fa5f1a854
Keuvain <sheepinadev@outlook.fr>
2015-12-12 15:31:46 +0100

    Changed the verification of the parameters

--- Pretzel.Image/ImageTag.cs
+++ Pretzel.Image/ImageTag.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿// Pretzel.Image plugin
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Linq;
-using System.Text.RegularExpressions;
 using DotLiquid;
 using Pretzel.Logic.Extensibility;
 
@@ -11,6 +11,8 @@ namespace Pretzel.Image
     [Export(typeof(ITag))]
     public class ImageTag : Tag, ITag
     {
+        private const string ExceptionMessage = "Expected syntax: {% img path/to/file [class(es)] [width [height]] %}";
+
         private string html;
 
         public new string Name => "Img";
@@ -21,66 +23,104 @@ namespace Pretzel.Image
 
             if (string.IsNullOrWhiteSpace(markup))
             {
-                throw new ArgumentException("Expected syntax: {% img path/to/file [class(es)] [width [height]] %}");
+                throw new ArgumentException(ExceptionMessage);
             }
 
-            var srcAttribute = string.Empty;
-            var widthAttribute = string.Empty;
-            var heightAttribute = string.Empty;
-            var classAttribute = string.Empty;
+            var attributes = new string[3];
 
             // The goal is to split the markup string in two : the part with the path and the part after the path.
             // This is surely not the best way to do it.
             var splittedMarkup = markup.Trim().SplitEnclosedInQuotes(new string[] { " " });
 
-            if (!splittedMarkup.Any())
+            if (!splittedMarkup.Any() || splittedMarkup.Count > 4)
             {
-                throw new ArgumentException("Expected syntax: {% img path/to/file [class(es)] [width [height]] %}");
+                throw new ArgumentException(ExceptionMessage);
             }
 
-            // String.Join cannot be used because it does not recover the possibles quotes.
-            var joined = markup.Replace(splittedMarkup[0], string.Empty).Replace("\"\"", string.Empty).Trim();
+            switch (splittedMarkup.Count)
+            {
+                case 2:
+                    attributes = ProcessOneArgument(splittedMarkup[1]);
+                    break;
 
-            srcAttribute = $"src=\"{splittedMarkup[0]}\"";
-            var regex = new Regex(@"^(?:(""[A-z\s]+""|[A-z]+))?(?:\s(\d+)(?:\s(\d+)+)?)?$|^(?:(\d+)(?:\s(\d+)+)?)?$");
-            var match = regex.Match(joined);
-            if (match.Success)
-            {
-                if (match.Groups[1].Success)
-                {
-                    classAttribute = $"class=\"{match.Groups[1].Value.Replace("\"", string.Empty)}\" ";
-                    if (match.Groups[2].Success)
-                    {
-                        widthAttribute = $" width={match.Groups[2].Value}";
-                        if (match.Groups[3].Success)
-                        {
-                            heightAttribute = $" height={match.Groups[3].Value}";
-                        }
-                    }
-                }
-                else
-                {
-                    if (match.Groups[4].Success)
-                    {
-                        widthAttribute = $" width={match.Groups[4].Value}";
-                        if (match.Groups[5].Success)
-                        {
-                            heightAttribute = $" height={match.Groups[5].Value}";
-                        }
-                    }
-                }
-            }
-            else
-            {
-                throw new ArgumentException("Expected syntax: {% img path/to/file [class(es)] [width [height]] %}");
+                case 3:
+                    attributes = ProcessTwoArguments(splittedMarkup);
+                    break;
+
+                case 4:
+                    attributes = ProcessThreeArguments(splittedMarkup);
+                    break;
             }
 
-            this.html = $"<img {classAttribute}{srcAttribute}{widthAttribute}{heightAttribute}></img>";
+            var srcAttribute = $"src=\"{splittedMarkup[0]}\"";
+
+            this.html = $"<img {attributes[0]}{srcAttribute}{attributes[1]}{attributes[2]}></img>";
         }
 
         public override void Render(Context context, System.IO.TextWriter result)
         {
             result.Write(this.html);
+        }
+
+        private static string[] ProcessOneArgument(string markup)
+        {
+            var values = new string[3];
+            int tmpStorage;
+
+            if (int.TryParse(markup, out tmpStorage))
+            {
+                values[1] = $" width={markup}";
+            }
+            else
+            {
+                values[0] = $"class=\"{markup}\" ";
+            }
+
+            return values;
+        }
+
+        private static string[] ProcessThreeArguments(IList<string> markup)
+        {
+            var values = new string[3];
+
+            int tmpStorage;
+
+            if (int.TryParse(markup[1], out tmpStorage) || !int.TryParse(markup[2], out tmpStorage) || !int.TryParse(markup[3], out tmpStorage))
+            {
+                throw new ArgumentException(ExceptionMessage);
+            }
+
+            values[0] = $"class=\"{markup[1]}\" ";
+            values[1] = $" width={markup[2]}";
+            values[2] = $" height={markup[3]}";
+
+            return values;
+        }
+
+        private static string[] ProcessTwoArguments(IList<string> markup)
+        {
+            var values = new string[3];
+
+            int tmpStorage;
+
+            // The second argument is either the width or the height.
+            if (!int.TryParse(markup[2], out tmpStorage))
+            {
+                throw new ArgumentException(ExceptionMessage);
+            }
+
+            if (int.TryParse(markup[1], out tmpStorage))
+            {
+                values[1] = $" width={markup[1]}";
+                values[2] = $" height={markup[2]}";
+            }
+            else
+            {
+                values[0] = $"class=\"{markup[1]}\" ";
+                values[1] = $" width={markup[2]}";
+            }
+
+            return values;
         }
     }
 }
